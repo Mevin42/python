4c7094457f4cf86004048bcfdbc6ab51072a6b4b
Mevin42 <sg.etecs@gmail.com>
2026-05-25 23:51:54 +0000

    pyton

---
+++ py.py
@@ -0,0 +1,8 @@
+class hospedes:
+    def __init__(self, nome, CPF, telefone, email, DDN):
+        self.nome = nome
+        self.CPF = CPF
+        self.telefone = telefone
+        self.email = email
+        self.DDN = DDN
+    
