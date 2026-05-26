ead6c1f2b9c1a28cb91ed6cfbd4b270bc24c8685
Mevin42 <sg.etecs@gmail.com>
2026-05-26 00:56:20 +0000

    apapappapapapathon

--- py.py
+++ py.py
@@ -6,3 +6,33 @@ class hospedes:
         self.email = email
         self.DDN = DDN
     
+class quarto:
+    def __init__(self, numero, status, andar, servico):
+        self.numero = n
+        self.status = true
+        self.andar = andar
+        self.servico = serv
+
+class reserva:
+    def __init__(self, numero, status, hospede, quarto, data, valor_total):
+        self.numero = Rn
+        self.status = true
+        self.hospede = hosp
+        self.quarto = quarto
+        self.data = data
+        self.valor_total = vtotal
+
+class pagamento:
+    def __init__(self, valor, forma, status):
+        self.valor = valor
+        self.forma = forma
+        self.status = true
+
+class hotel:
+    def __init__(self, nome, endereco, avaliacao, telefone, email, validacao):
+        self.nome = nome
+        self.endereco = endereco
+        self.avaliacao = ava
+        self.telefone = telefone
+        self.email = email
+        self.validacao = false
